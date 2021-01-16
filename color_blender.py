be59a880443f46eef6b2596e803019d8da0ba3cb
anair3007 <anair3007@gmail.com>
2021-01-16 18:38:10 -0500

    initial skeleton

---
+++ color_blender.py
@@ -0,0 +1,24 @@
+# Import required modules
+### Your code here
+
+# Create a white canvas
+def createCanvas(color):
+    ### Your code here
+
+# Display an image
+def displayImage(image):
+    ### Your code here
+
+# Load the color palette
+### Your code here
+
+# Create canvas
+# Use the createCanvas function
+# you have written above
+### Your code here
+
+# Display the canvas and the
+# color palette
+# Use the functions you have
+# written above
+### Your code here
